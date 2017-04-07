74994dae1cffb47f1d5ac359801479915808e6e7
Gerardo Leal <gerard.leal91@gmail.com>
2017-04-07 18:00:21 -0300

    Initial Commit

---
+++ app/solution/solution.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+   moduleId: __moduleName,
+   selector: 'my-solution',
+   templateUrl: 'solution.component.html'
+})
+export class SolutionComponent { }
